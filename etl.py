fd96fe6ae88f6689074c71f6b9e014fdc66c8cd6
João Antônio Corrêa <joaoantoniocorreaf@gmail.com>
2025-07-01 21:59:52 -0300

    feat: add dataset dictionary with Boston data CSV links

--- etl.py
+++ etl.py
@@ -8,3 +8,14 @@ load_dotenv()
 
 DATA_DIR = "data"
 os.makedirs(DATA_DIR, exist_ok=True)
+
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
+
+DATASETS = {
+    "2025": "https://data.boston.gov/dataset/8048697b-ad64-4bfc-b090-ee00169f2323/resource/9d7c2214-4709-478a-a2e8-fb2020a5bb94/download/tmpxvr3p2yd.csv",
+    "2024": "https://data.boston.gov/dataset/8048697b-ad64-4bfc-b090-ee00169f2323/resource/dff4d804-5031-443a-8409-8344efd0e5c8/download/tmpm461rr5o.csv",
+    "2023": "https://data.boston.gov/dataset/8048697b-ad64-4bfc-b090-ee00169f2323/resource/e6013a93-1321-4f2a-bf91-8d8a02f1e62f/download/tmpwbgyud93.csv",
+    "2022": "https://data.boston.gov/dataset/8048697b-ad64-4bfc-b090-ee00169f2323/resource/81a7b022-f8fc-4da5-80e4-b160058ca207/download/tmpfm8veglw.csv",
+    "2021": "https://data.boston.gov/dataset/8048697b-ad64-4bfc-b090-ee00169f2323/resource/f53ebccd-bc61-49f9-83db-625f209c95f5/download/tmp88p9g82n.csv",
+    "2020": "https://data.boston.gov/dataset/8048697b-ad64-4bfc-b090-ee00169f2323/resource/6ff6a6fd-3141-4440-a880-6f60a37fe789/download/tmpcv_10m2s.csv",
+}
